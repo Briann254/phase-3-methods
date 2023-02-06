0239a17537e7c17cb27971e4e75d9cbf64b87ec1
git_bnderitu <bnnderitu01@email.com>
2023-02-06 22:53:00 +0300

    Completed methods lab

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer"
+end
+
+greet_programmer
+
+
+def greet(name = "Naureen");
+  puts "Hello, #{name}"
+end
+
+greet
+
+
+def greet_with_default(name = "programmer!")
+  puts "Hello, name!"
+end
+greet_with_default
+
+
+def add(num1, num2)
+  return num1 + num2
+end
+
